13ddcbfe0e0ea5f6a108c94a48fe8db389af94c1
PwzXxm <cepheidfov@gmail.com>
2019-04-06 23:28:18 +1100

    formatted code and added args switch

--- src/Goat.hs
+++ src/Goat.hs
@@ -9,22 +9,29 @@ import System.Exit
 
 main :: IO ()
 main
-  = do { progname <- getProgName
-        ; args <- getArgs
-        ; checkArgs progname args
-        ; input <- readFile (head args)
-        ; let output = runGoatParse input
-        ; case output of
-            Right ast -> print ast
-            Left  err -> do { putStr "Parse error at "
-                            ; print err
-                            }
-        }
+  = do
+      progname <- getProgName
+      args <- getArgs
+      excute progname args
 
-checkArgs :: String -> [String] -> IO ()
-checkArgs _ [filename]
-    = return ()
-checkArgs progname _
-    = do { putStrLn ("Usage: " ++ progname ++ " filename\n\n")
-        ; exitWith (ExitFailure 1)
-        }
+excute :: String -> [String] -> IO ()
+excute _ [filename]
+  = do
+    putStrLn "Sorry, cannot generate code yet"
+    exitWith (ExitFailure 1)
+
+excute _ ["-p", filename]
+  = do
+      input <- readFile filename
+      let output = runGoatParse input
+      case output of
+        Right ast -> print ast
+        Left  err -> do 
+          putStr "Parse error at "
+          print err
+    -- TODO: Pretty print
+
+excute progname _
+  = do
+      putStrLn ("Usage: " ++ progname ++ " filename\n\n")
+      exitWith (ExitFailure 1)
