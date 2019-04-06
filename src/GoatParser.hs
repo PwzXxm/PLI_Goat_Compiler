2f9c47ec2863fed554c227bea427a03b805b3e71
Zijun Chen <c.zijunchen@gmail.com>
2019-04-06 22:47:55 +1100

    Added skeleton draft

--- src/GoatParser.hs
+++ src/GoatParser.hs
@@ -4,6 +4,7 @@ import GoatAST
 
 import Data.Char
 import Text.Parsec
+import Text.Parsec.Pos
 import System.Environment
 import System.Exit
 
@@ -13,17 +14,35 @@ type Parser a
 -- runGoatPaser :: ???
 -- runGoatPaser = runParser pMain 0 ""
 
+reserved :: Tok -> Parser ()
+reserved tok
+  = gToken (\t -> if t == tok then Just () else Nothing)
+
+
+gToken :: (Tok -> Maybe a) -> Parser a
+gToken test
+  = token showToken posToken testToken
+    where
+      showToken (pos, tok) = show tok
+      -- TODO: get the actual pos
+      posToken  (pos, tok) = newPos "" 0 0
+      testToken (pos, tok) = test tok
+
+  
 
 pMain :: Parser GoatProgram
 pMain
   = do
+    reserved PROC
     return Program
     --   p <- pProg
     --   return p
 
 
-testdata = [(0,PROC),(0,IDENT "main"),(0,LPAREN),(0,RPAREN),(0,INT),(0,IDENT "a"),(0,SEMI),(0,INT),(0,IDENT "b"),(0,SEMI),(0,BEGIN),(0,IDENT "a"),(0,ASSIGN),(0,INT_CONST 2),(0,MUL),(0,LPAREN),(0,INT_CONST 1),(0,PLUS),(0,INT_CONST 10),(0,RPAREN),(0,PLUS),(0,INT_CONST 2),(0,PLUS),(0,INT_CONST 2),(0,MUL),(0,INT_CONST 2),(0,PLUS),(0,INT_CONST 14),(0,SEMI),(0,IDENT "b"),(0,ASSIGN),(0,MINUS),(0,IDENT "a"),(0,PLUS),(0,IDENT "a"),(0,SEMI),(0,END)]
-test = runParser pMain () "" testdata
+testdata1 = [(0,PROC), (0,IDENT "main"),(0,LPAREN),(0,RPAREN),(0,INT),(0,IDENT "a"),(0,SEMI),(0,INT),(0,IDENT "b"),(0,SEMI),(0,BEGIN),(0,IDENT "a"),(0,ASSIGN),(0,INT_CONST 2),(0,MUL),(0,LPAREN),(0,INT_CONST 1),(0,PLUS),(0,INT_CONST 10),(0,RPAREN),(0,PLUS),(0,INT_CONST 2),(0,PLUS),(0,INT_CONST 2),(0,MUL),(0,INT_CONST 2),(0,PLUS),(0,INT_CONST 14),(0,SEMI),(0,IDENT "b"),(0,ASSIGN),(0,MINUS),(0,IDENT "a"),(0,PLUS),(0,IDENT "a"),(0,SEMI),(0,END)]
+testdata2 = [(0,RPAREN),(0,INT),(0,IDENT "a")]
+test1 = runParser pMain () "" testdata1
+test2 = runParser pMain () "" testdata2
 
 
 -- temp
