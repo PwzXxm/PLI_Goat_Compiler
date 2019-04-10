96d5d5468aedde4bbae0e69c8244d33c51a06ad3
dodojk <zhet1@student.unimelb.edu.au>
2019-04-11 02:56:20 +1000

    finish decls

--- src/GoatFormatter.hs
+++ src/GoatFormatter.hs
@@ -3,11 +3,76 @@ module GoatFormatter where
 import GoatAST
 
 runGoatFormatter :: GoatProgram -> IO ()
+runGoatFormatter (Program []) = return ()
 runGoatFormatter (Program (x:xs))
   = do
-      putStrLn (show x)
+      programFormatter x
       runGoatFormatter (Program xs)
 
-runGoatFormatter (Program [])
+programFormatter :: Proc -> IO ()
+programFormatter (Proc id paras decls stmts)
   = do
-      return ()
+      putStr "proc "
+      putStr id
+      putStr "("
+      parasFormatter paras
+      putStr ")\n"
+      declsFormatter decls
+      -- stmtsFormatter stmts
+
+
+parasFormatter :: [Para] -> IO ()
+parasFormatter [x] = paraFormatter x
+parasFormatter (x:xs)
+  = do
+      paraFormatter x
+      putStr ", "
+      parasFormatter xs
+
+paraFormatter :: Para -> IO ()
+paraFormatter (Para id btype indi)
+  = do
+      indiFormatter indi
+      putStr " "
+      btypeFormatter btype
+      putStr " "
+      putStr id
+
+declsFormatter :: [Decl] -> IO ()
+declsFormatter [] = return ()
+declsFormatter ((Decl id btype shape):xs)
+  = do
+      putStr "    "
+      btypeFormatter btype
+      putStr " "
+      putStr id
+      shapeFormatter shape
+      putStr "\n"
+      declsFormatter xs
+      
+      
+
+-- stmtsFormatter :: [Stmt] -> IO ()
+
+indiFormatter :: Indi -> IO ()
+indiFormatter InVar = putStr "var"
+indiFormatter InRef = putStr "ref"
+
+btypeFormatter :: BaseType -> IO ()
+btypeFormatter BoolType = putStr "bool"
+btypeFormatter IntType = putStr "int"
+
+shapeFormatter :: Shape -> IO ()
+shapeFormatter ShapeVar = return ()
+shapeFormatter (ShapeArr a)
+  = do
+    putStr "["
+    putStr (show a)
+    putStr "]"
+shapeFormatter (ShapeMat a b)
+  = do
+    putStr "["
+    putStr (show a)
+    putStr ", "
+    putStr (show b)
+    putStr "]"
