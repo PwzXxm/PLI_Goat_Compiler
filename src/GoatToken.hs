e638c0c4a8eefb660f5cc1de237d5f85317cd90b
PwzXxm <cepheidfov@gmail.com>
2019-04-14 21:50:41 +1000

    formatted GoatToken.hs

--- src/GoatToken.hs
+++ src/GoatToken.hs
@@ -5,11 +5,49 @@ import           Text.Parsec.Pos
 type Token = (SourcePos, Tok)
 
 data Tok
-  = BOOL | INT | FLOAT | PROC | BEGIN | END | READ | WRITE | ELSE | IF
-  | FI | DO | OD | VAL | WHILE | REF | THEN | CALL | ASSIGN | INT_CONST Int
-  | BOOL_CONST Bool | FLOAT_CONST Float | IDENT String | LIT String
-  | LPAREN | RPAREN | PLUS | MINUS | MUL | DIV | LESS | LESSEQUAL | GREATER
-  | GREATEQUAL | EQUAL | UNEQUAL | LSQUARE | RSQUARE | OR | AND | UNARYNOT | SEMI | COMMA
+  = BOOL
+  | INT
+  | FLOAT
+  | PROC
+  | BEGIN
+  | END
+  | READ
+  | WRITE
+  | ELSE
+  | IF
+  | FI
+  | DO
+  | OD
+  | VAL
+  | WHILE
+  | REF
+  | THEN
+  | CALL
+  | ASSIGN
+  | INT_CONST Int
+  | BOOL_CONST Bool
+  | FLOAT_CONST Float
+  | IDENT String
+  | LIT String
+  | LPAREN
+  | RPAREN
+  | PLUS
+  | MINUS
+  | MUL
+  | DIV
+  | LESS
+  | LESSEQUAL
+  | GREATER
+  | GREATEQUAL
+  | EQUAL
+  | UNEQUAL
+  | LSQUARE
+  | RSQUARE
+  | OR
+  | AND
+  | UNARYNOT
+  | SEMI
+  | COMMA
   deriving (Eq)
 
 -- showTok :: (Show a) => Tok a -> String
