a39fed0089f4e53e8c3d31e2490dc0cdfa62b7da
Zijun Chen <c.zijunchen@gmail.com>
2019-05-23 11:34:20 +1000

    Add interface for GoatCodeGenerator.

--- src/GoatCodeGenerator.hs
+++ src/GoatCodeGenerator.hs
@@ -2,44 +2,33 @@ module GoatCodeGenerator where
 
 import Control.Monad.State
 import           Data.Monoid
+import OzInstruction
+import GoatAST
 
-data Astate = Astate { regCounter :: Int, ins :: Endo [String]}
 
-type Analyzer a = State Astate a
+data Gstate = Gstate { regCounter :: Int, instructions :: Endo [Instruction]}
+
+type Analyzer a = State Gstate a
 
 -- O(1) time complexity
 -- Idea from https://kseo.github.io/posts/2017-01-21-writer-monad.html
-output :: String -> Analyzer ()
-output x
+appendIns :: Instruction -> Analyzer ()
+appendIns x
   = do
       st <- get
-      put st{ins = (( (ins st) <> (Endo ([x]<>))  )) }
-      return ()
+      put st{instructions = (( (instructions st) <> (Endo ([x]<>))  )) }
 
+----------------------------------------------
 
-
-
-codeGenerator :: Int -> Analyzer [Char]
-codeGenerator 0
+genProgram :: DGoatProgram -> Analyzer ()
+genProgram _ 
   = do 
-      output "11111\n"
-      return "aaaa"
+      appendIns (Comment "AAAA")
 
-codeGenerator n
-  = do
-      output "11111\n"
-      x <- codeGenerator (n-1)
-      return x
+runCodeGenerator :: DGoatProgram -> [Instruction]
+runCodeGenerator dGoatProgram
+  = let state = Gstate { regCounter = 0, instructions = Endo ([]<>) }
+        (_, s) = runState (genProgram dGoatProgram) state
+    in (appEndo (instructions s)) []
 
-
-test :: IO ()
-test
-  = do
-      let
-        state = Astate
-          { regCounter = 0, ins = Endo ([]<>) }
-      let (r, s) = runState (codeGenerator 100000) state
-      putStrLn r
-      putStrLn (concat (appEndo (ins s) []))
-      return ()
-  
+test = runCodeGenerator (DProgram 0 [])
