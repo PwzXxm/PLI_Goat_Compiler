0130b30098573f52413ab32340724020d53f442f
Zijun Chen <c.zijunchen@gmail.com>
2019-05-28 15:09:50 +1000

    refactor forwardScanner.

--- src/GoatOptimizer.hs
+++ src/GoatOptimizer.hs
@@ -14,70 +14,43 @@ isComment _ = False
 
 runOptimizer :: [Instruction] -> [Instruction]
 runOptimizer insList
-  = let state = Ostate { lastIns = IComment "placeholder", instructions = Endo ([]<>) }
-        s = execState (mainOptimizer insList) state
-    in backwardScanner ((appEndo (instructions s)) [])
+  = backwardScanner $ forwardScanner insList
 
-data Ostate = Ostate 
-  { lastIns :: Instruction, instructions :: Endo [Instruction]}
+forwardScanner :: [Instruction] -> [Instruction]
+forwardScanner insList
+  = forwardScanner_ (IComment "placeholder") insList
 
-type Optimizer a = State Ostate a
-
--- O(1) time complexity
--- Idea from https://kseo.github.io/posts/2017-01-21-writer-monad.html
-appendIns :: Instruction -> Optimizer ()
-appendIns x
-  = do
-      st <- get
-      put st{instructions = (( (instructions st) <> (Endo ([x]<>))  )) }
-
-getLastIns :: Optimizer Instruction
-getLastIns
-  = do
-      st <- get
-      return (lastIns st)
-
-updateLastIns :: Instruction -> Optimizer ()
-updateLastIns ins
-  = case ins of
-      -- ignore comments
-      (IComment _) -> return ()
-      ins -> do
-        st <- get
-        put st{lastIns = ins}
-
-
-mainOptimizer :: [Instruction] -> Optimizer ()
-mainOptimizer insList0
-  = do
-      mapM_ forwardScanner insList0
-    
-forwardScanner :: Instruction -> Optimizer ()
-forwardScanner ins
-  = do
-      last <- getLastIns
-      updateLastIns ins
-
-      -- (last ins, current ins)
-      let insPair = (last, ins)
-      case insPair of
+forwardScanner_ :: Instruction -> [Instruction] -> [Instruction]
+forwardScanner_ lastIns (ins:insList)
+  = let 
+      newLast = case ins of 
+        (IComment _) -> lastIns
+        _            -> ins
+      -- (previous, current)
+      listPair = (lastIns, ins)
+      recursion = forwardScanner_ newLast insList
+    in
+      case listPair of
         (IStatement (Store slot0 reg0), IStatement (Load reg1 slot1)) | slot0 == slot1
-          -> if reg0 == reg1
-              then return ()
-              else appendIns (IOperation (Move reg1 reg0))
+          -> if reg0 == reg1 then recursion else ((IOperation (Move reg1 reg0)):recursion)
         _
-          -> appendIns ins
+          -> (ins:recursion)
+
+forwardScanner_ _ [] = []
 
 
 backwardScanner :: [Instruction] -> [Instruction]
 backwardScanner (ins:insList)
   -- (current, next)
-  = let insPair = (ins, nextActualIns insList) in
-    case insPair of 
-      (IBranch (Uncond label0), ILabel label1) | label0 == label1
-        -> backwardScanner insList
-      _
-        -> (ins:backwardScanner insList)
+  = let 
+      insPair = (ins, nextActualIns insList)
+      recursion = backwardScanner insList
+    in
+      case insPair of 
+        (IBranch (Uncond label0), ILabel label1) | label0 == label1
+          -> recursion
+        _
+          -> (ins:recursion)
 
 backwardScanner [] = []
 
