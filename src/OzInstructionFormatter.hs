1e8d839ee8aecfc464f6d5be4a225011a80818e1
Zijun Chen <c.zijunchen@gmail.com>
2019-05-28 22:36:17 +1000

    update.

--- src/OzInstructionFormatter.hs
+++ src/OzInstructionFormatter.hs
@@ -16,24 +16,24 @@ instructionFormatter x
 
 -- | Print one instruction without indentation
 instructionFormatter_ :: Instruction -> String
-instructionFormatter_ (IConstant c) = constantFormatter c
+instructionFormatter_ (IConstant c)  = constantFormatter c
 instructionFormatter_ (IOperation o) = operationFormatter o
 instructionFormatter_ (IStatement s) = statementFormatter s
-instructionFormatter_ (IDebug d) = debugFormatter d
-instructionFormatter_ (IBranch b) = branchFormatter b
-instructionFormatter_ (ICall l) = "call " ++ (lshow l)
-instructionFormatter_ (ICall_bt bt) = "call_builtin " ++ bt
+instructionFormatter_ (IDebug d)     = debugFormatter d
+instructionFormatter_ (IBranch b)    = branchFormatter b
+instructionFormatter_ (ICall l)      = "call " ++ (lshow l)
+instructionFormatter_ (ICall_bt bt)  = "call_builtin " ++ bt
 instructionFormatter_ (IPushStack i) = "push_stack_frame " ++ (show i)
-instructionFormatter_ (IPopStack i) = "pop_stack_frame " ++ (show i)
+instructionFormatter_ (IPopStack i)  = "pop_stack_frame " ++ (show i)
 instructionFormatter_ (IComment str) = "# " ++ str
-instructionFormatter_ (IHalt) = "halt"
-instructionFormatter_ (IReturn) = "return"
-instructionFormatter_ (ILabel l) = (lshow l) ++ ":"
+instructionFormatter_ (IHalt)        = "halt"
+instructionFormatter_ (IReturn)      = "return"
+instructionFormatter_ (ILabel l)     = (lshow l) ++ ":"
 
 -- | Print constant
 constantFormatter :: Constant -> String
-constantFormatter (ConsInt r i) = "int_const " ++ (rshow r) ++ ", " ++ (show i)
-constantFormatter (ConsFloat r f) = "real_const " ++ (rshow r) ++ ", " ++ (show f)
+constantFormatter (ConsInt r i)    = "int_const " ++ (rshow r) ++ ", " ++ (show i)
+constantFormatter (ConsFloat r f)  = "real_const " ++ (rshow r) ++ ", " ++ (show f)
 constantFormatter (ConsString r s) = "string_const " ++ (rshow r) ++ ", \"" ++ s ++ "\""
 
 
@@ -47,11 +47,11 @@ threeFormatter r1 r2 r3 = (rshow r1) ++ ", " ++ (rshow r2) ++ ", " ++ (rshow r3)
 operationFormatter :: Operation -> String
 operationFormatter (Add_off r1 r2 r3) = "add_offset " ++ (threeFormatter r1 r2 r3)
 operationFormatter (Sub_off r1 r2 r3) = "sub_offset " ++ (threeFormatter r1 r2 r3)
-operationFormatter (And_ r1 r2 r3) = "and " ++ (threeFormatter r1 r2 r3)
-operationFormatter (Or_ r1 r2 r3) = "or " ++ (threeFormatter r1 r2 r3)
-operationFormatter (Not_ r1 r2) = "not " ++ (twoFormatter r1 r2)
-operationFormatter (Int2real r1 r2) = "int_to_real " ++ (twoFormatter r1 r2)
-operationFormatter (Move r1 r2) = "move " ++ (twoFormatter r1 r2)
+operationFormatter (And_ r1 r2 r3)    = "and " ++ (threeFormatter r1 r2 r3)
+operationFormatter (Or_ r1 r2 r3)     = "or " ++ (threeFormatter r1 r2 r3)
+operationFormatter (Not_ r1 r2)       = "not " ++ (twoFormatter r1 r2)
+operationFormatter (Int2real r1 r2)   = "int_to_real " ++ (twoFormatter r1 r2)
+operationFormatter (Move r1 r2)       = "move " ++ (twoFormatter r1 r2)
 operationFormatter (Binary op t r1 r2 r3)
   = (binaryOpFormatter op)
     ++ "_" ++
@@ -70,12 +70,12 @@ binaryOpFormatter Add = "add"
 binaryOpFormatter Sub = "sub"
 binaryOpFormatter Mul = "mul"
 binaryOpFormatter Div = "div"
-binaryOpFormatter Eq = "cmp_eq"
-binaryOpFormatter Ne = "cmp_ne"
-binaryOpFormatter Gt = "cmp_gt"
-binaryOpFormatter Ge = "cmp_ge"
-binaryOpFormatter Lt = "cmp_lt"
-binaryOpFormatter Le = "cmp_le"
+binaryOpFormatter Eq  = "cmp_eq"
+binaryOpFormatter Ne  = "cmp_ne"
+binaryOpFormatter Gt  = "cmp_gt"
+binaryOpFormatter Ge  = "cmp_ge"
+binaryOpFormatter Lt  = "cmp_lt"
+binaryOpFormatter Le  = "cmp_le"
 
 unaryOpFormatter :: UnaryOp -> String
 unaryOpFormatter NEG = "neg"
@@ -86,10 +86,10 @@ typeFormatter INT = "int"
 typeFormatter REAL = "real"
 
 statementFormatter :: Statement -> String
-statementFormatter (Store i r) = "store " ++ (show i) ++ ", " ++ (rshow r)
-statementFormatter (Load r i) = "load " ++ (rshow r) ++ ", " ++ (show i)
-statementFormatter (Load_ad r i) = "load_address " ++ (rshow r) ++ ", " ++ (show i)
-statementFormatter (Load_in r1 r2) = "load_indirect " ++ (twoFormatter r1 r2)
+statementFormatter (Store i r)      = "store " ++ (show i) ++ ", " ++ (rshow r)
+statementFormatter (Load r i)       = "load " ++ (rshow r) ++ ", " ++ (show i)
+statementFormatter (Load_ad r i)    = "load_address " ++ (rshow r) ++ ", " ++ (show i)
+statementFormatter (Load_in r1 r2)  = "load_indirect " ++ (twoFormatter r1 r2)
 statementFormatter (Store_in r1 r2) = "store_indirect " ++ (twoFormatter r1 r2)
 
 rshow :: Reg -> String
@@ -99,7 +99,7 @@ lshow :: Label -> String
 lshow l = "label_" ++ l
 
 debugFormatter :: Debug -> String
-debugFormatter (DebugReg r) = "debug_reg " ++ (rshow r)
+debugFormatter (DebugReg r)  = "debug_reg " ++ (rshow r)
 debugFormatter (DebugSlot i) = "debug_slot " ++ (show i)
 debugFormatter DebugStack = "debug_stack"
 
